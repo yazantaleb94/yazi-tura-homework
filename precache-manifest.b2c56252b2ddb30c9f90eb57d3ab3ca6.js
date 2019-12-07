e30d3dc1392564a4a01976dad2b6da8b5fed2d89
umuttalipoglu94 <yazan.taleb@ogr.sakarya.edu.tr>
2019-12-07 16:52:42 +0300

    Updates

---
+++ precache-manifest.b2c56252b2ddb30c9f90eb57d3ab3ca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afcfa2584ed4407b62c4924fad730da",
+    "url": "/yaziTura/index.html"
+  },
+  {
+    "revision": "8db58dfb6bebde4d5c47",
+    "url": "/yaziTura/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "4f9f91fff8131483efcf",
+    "url": "/yaziTura/static/js/2.5af7eab3.chunk.js"
+  },
+  {
+    "revision": "8db58dfb6bebde4d5c47",
+    "url": "/yaziTura/static/js/main.82be9fcb.chunk.js"
+  },
+  {
+    "revision": "33bf9ee0b85289eb2b7d",
+    "url": "/yaziTura/static/js/runtime~main.0857b46c.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/yaziTura/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/yaziTura/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
