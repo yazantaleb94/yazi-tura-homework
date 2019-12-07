b310a5db765fad22adda27022fdf174ccdd3c167
umuttalipoglu94 <yazan.taleb@ogr.sakarya.edu.tr>
2019-12-07 16:58:07 +0300

    Updates

---
+++ precache-manifest.42f09151014284a0e51bfce3df31741e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3496bbfbc987829a81bc7e88291e5e96",
+    "url": "/yazi-tura-homework/index.html"
+  },
+  {
+    "revision": "d2923b21ca1ea51b4c50",
+    "url": "/yazi-tura-homework/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "4f9f91fff8131483efcf",
+    "url": "/yazi-tura-homework/static/js/2.5af7eab3.chunk.js"
+  },
+  {
+    "revision": "d2923b21ca1ea51b4c50",
+    "url": "/yazi-tura-homework/static/js/main.bb5df3e5.chunk.js"
+  },
+  {
+    "revision": "88b9f313409e895000bd",
+    "url": "/yazi-tura-homework/static/js/runtime~main.0a62a07d.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/yazi-tura-homework/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/yazi-tura-homework/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
